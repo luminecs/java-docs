fa63a63d8aa1e86253a59c99b8bae6efe3803909
luminecs <wangxuke.china@gmail.com>
2023-10-17 20:11:38 +0800

    commit

--- java-tutorial/src/main/java/com/mycompany/app/io/bytestreams/CopyBytes.java
+++ java-tutorial/src/main/java/com/mycompany/app/io/bytestreams/CopyBytes.java
@@ -6,11 +6,13 @@ import java.io.IOException;
 
 public class CopyBytes {
     public static void main(String[] args) throws IOException {
+        String appRootPath = System.getProperty("user.dir");
+
         FileInputStream in = null;
         FileOutputStream out = null;
         try {
-            in = new FileInputStream("xanadu.txt");
-            out = new FileOutputStream("outagain.txt");
+            in = new FileInputStream(appRootPath + "/essential/io/xanadu.txt");
+            out = new FileOutputStream(appRootPath + "/essential/io/outagain.txt");
             int c;
             while ((c = in.read()) != -1) {
                 out.write(c);
